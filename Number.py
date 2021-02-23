96ec4fa729818538187e340d7f74f97fe8d70474
Nikos <nikospoursalidis@>
2021-02-23 19:20:15 +0200

    Created Prime(n) function

--- Number.py
+++ Number.py
@@ -1,33 +1,12 @@
-
-def divisors(n):
-    return cast(n)[0]
-def primes(n):
-    cast(n)[1]
-def relPrimes(n):
-    primes = []
-    for i in range(2, n+1):
-        if commonDiv(i, n) == [1]:
-            primes.append(i)
-    return primes
-def commonDiv(a, b):
-    divs = []
-    primes = []
-    for k in divisors(a):
-        if k in divisors(b):
-            divs.append(k)
-    return divs
-def cast(n):
-    divisors = []
-    primes = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
-            burn = 0
-            for j in range(2, i):
-                if commonDiv(i, j) == [1]:
-                    continue
-                else:
-                    burn+=1 
-            if burn == 2:
-                primes.append(i)
-    return divisors, primes
+n=20
+def Primes(n):
+    Primes=[]
+    for i in range(2,n):
+        divs=[]
+        for j in range(2,i):
+            if i % j == 0:
+                divs.append(j)
+        if divs == []:
+            Primes.append(i)
+    return Primes           
+print(Primes(n))
